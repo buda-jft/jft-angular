010e5dc873a000b823b5a30882973980831609c9
buda-jft <y.singh@jellyfishtechnologies.com>
2021-03-03 18:16:48 +0530

    part7 event handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  months = ["January", "February", "March", "April", "May",
+  "June", "July", "August", "September",
+  "October", "November", "December"];
+
+  isavailable = true;
+
+  myClickFunc(event) {
+  //just added console.log which will display the event details in browser on click of the button.
+    alert("Button is clicked");
+    console.log(event);
+  }
+  changemonths(event) {
+    console.log(event);
+  }
 }

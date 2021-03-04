fb425c4222669ba69f8d5f0ae151fba571c28f55
buda-jft <y.singh@jellyfishtechnologies.com>
2021-03-04 13:15:29 +0530

    part11 routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {ContactusComponent} from './contactus/contactus.component';
+const routes: Routes = [
+  // this holds all the routes in our project
+  {path: "home", component: HomeComponent},
+  {path: "contactus", component: ContactusComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [HomeComponent, ContactusComponent];

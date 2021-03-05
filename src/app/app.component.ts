50f0c262f4791e583d73f9a0b1c3661acdccd294
buda-jft <y.singh@jellyfishtechnologies.com>
2021-03-05 14:58:30 +0530

    animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,54 @@
-import { Component } from '@angular/core';
-import {MyserviceService} from './myservice.service';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from
-'@angular/cdk/drag-drop';
-// import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { MyserviceService } from "./myservice.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
+// import {CdkDragDrop, moveItemInArray, transferArrayItem} from
+// '@angular/cdk/drag-drop';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  styles: [
+    `
+  div{
+    margin: 0atuo;
+    text-align; center;
+    width:200px;
+  }
+  .rotate{
+    width:100px;
+    height:100px;
+    border:solid 1px red;  
+  }
+  `,
+  ],
+  animations: [
+    trigger("myanimation", [
+      state(
+        "smaller",
+        style({
+          transform: "translateY(100px)",
+        })
+      ),
+      state(
+        "larger",
+        style({
+          transform: "translateY(0px)",
+        })
+      ),
+      transition("smaller <=> larger", animate("300ms ease-in")),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'my-first-app';
-  public persondetails = [];
-  constructor(private myService: MyserviceService) {}
-  ngOnInit() {
-    this.myService.getData().subscribe((data) => {
-      this.persondetails = Array.from(Object.keys(data), k=>data[k]);
-      console.log(this.persondetails);
-    })
-  }
-
-  onDrop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    }
+  state: string = "smaller";
+  animate() {
+    this.state = this.state == 'larger' ? 'smaller' : 'larger';
   }
 }
